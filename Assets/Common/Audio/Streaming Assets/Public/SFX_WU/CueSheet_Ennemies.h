8985b05845560a1dad1a833bd17bfa55288be390
JeanHumphris <jeanhumphrisbox@gmail.com>
2026-02-01 14:19:35 +0900

    Full_SD Push

---
+++ Assets/Common/Audio/Streaming Assets/Public/SFX_WU/CueSheet_Ennemies.h
@@ -0,0 +1,27 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : GGJ_Game_Audio
+ *  Tool Ver.        : Ver.3.56.01
+ *  ACB  Ver.        : Ver.1.46.0
+ *  File Path        : C:/Gaming/Game Dev/CAMELEON_GGJ/Assets/Common/Audio/Streaming Assets/Public/SFX_WU
+ *  File Name        : CueSheet_Ennemies.h
+ *  File Size        : 216448 bytes
+ *  Date Time        : 2026/02/01 14:19:08
+ *  Target           : Public
+ *  Cues             : 1
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : C:/Gaming/Game Dev/CAMELEON_GGJ/Assets/Common/Audio/Streaming Assets/Public/SFX_WU/CueSheet_Ennemies.awb
+ *===========================================================================*/
+
+#define CRI_CUESHEET_ENNEMIES_CUENUM (1)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_CUESHEET_ENNEMIES_HUMAN_READ_BOOK      ( 1) /*  */
+
+/* Block List (Block Index) */
+#define CRI_CUESHEET_ENNEMIES_BLOCK_HUMAN_READ_BOOK_BLOCK_HUMAN_READ_BOOK ( 0) /*  */
+
+/* end of file */
+
